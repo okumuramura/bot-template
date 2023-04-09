3e909bd1d145b064e0f0e26da9f43742db421725
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-04-09 22:41:04 +0400

    Add Functional
    added some functional,
    added /help structure and beautiful output

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ bot = Bot('my-bot')
 dispatcher = CliDispatcher(bot)
 
 
-@bot.command('hello')
+@bot.command('hello', description="Выводит в чат приветствие пользователя", example_output="Hello, {Username}")
 def hello_handler(msg: Message, args: List[str]) -> str:
     return f'Hello, {msg.author}!'
+
+
+@bot.command('sum', args_name_list=["<a>", "<b>"], description="Выводит в чат сумму двух чисел", example_output="2 + 3 = 5")
+def hello_handler(msg: Message, args: List[str]) -> str:
+    return f'{float(args[0])} + {float(args[1])} = {float(args[0]) + float(args[1])}'
 
 
 if __name__ == '__main__':
