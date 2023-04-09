3f7c246a7a8b4eee99d3887b8b884e5fa319f624
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-04-09 21:21:50 +0400

    refactored logic of input and output in bot
    It's my opinion about composition of input and output in user messages handler

--- bot/dispatcher/cli_dispatcher.py
+++ bot/dispatcher/cli_dispatcher.py
@@ -4,26 +4,35 @@ from bot.bot import Bot
 from bot.models.message import Message
 
 
+def cli_output(text: str, *args, **kwargs) -> None:
+    print(text, *args, **kwargs)
+
+
 class CliDispatcher:
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.message_id = 0
 
     def start_polling(self) -> None:
+        cli_output(f'\nHi, i am bot {self.bot.name}, write /help to get all my commands')
         try:
             self.__loop()
         except KeyboardInterrupt:
-            print('bye :)')
+            cli_output('bye :)')
 
     def __loop(self) -> None:
-        print(f'\nHi, i am bot {self.bot.name}, write /help to get all my commands')
         while True:
             line = input('User > | ')
-            msg = Message(
-                id=self.message_id,
-                text=line,
-                author=os.getlogin()
-            )
-            answer = self.bot.message(msg)
-            if answer is not None:
-                print(f'{self.bot.name} |', answer.text)
+            answer = self.msg_handler(line)
+            return answer
+
+    def msg_handler(self, line: str):
+        msg = Message(
+            id=self.message_id,
+            text=line,
+            author=os.getlogin()
+        )
+        answer = self.bot.message(msg)
+        if answer is not None:
+            cli_output(f'{self.bot.name} |', answer.text)
+        return answer
