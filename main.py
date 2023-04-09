98df08279865ae23a5b3cd452ce62ea7ade9a0de
okumuramura <bugagaga88@gmail.com>
2023-04-09 12:46:12 +0400

    handler decorators

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ bot = Bot('my-bot')
 dispatcher = CliDispatcher(bot)
 
 
+@bot.command('hello')
 def hello_handler(msg: Message, args: List[str]) -> str:
     return f'Hello, {msg.author}!'
 
 
-bot.register_handler('hello', hello_handler)
+# bot.register_handler('hello', hello_handler)
 
 if __name__ == '__main__':
     dispatcher.start_polling()
