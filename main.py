b1f2aa776888a63e87d5fef3e0314c8349b2274c
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-04-16 10:12:10 +0400

    vk dispatcher commit

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from typing import List
 from bot.bot import Bot
 from bot.models.message import Message
 from bot.dispatcher.cli_dispatcher import CliDispatcher
+from bot.dispatcher.vk_despatcher import VkDispatcher
 
+vk_api_token = '''vk1.a.URgckFWnoKOkZAN1qX_QifHRC_g5xLT0M3gO0-CH4RH4D-BP-PokvRYi9pJgRN21xqBfgOo09
+-Vs_mEbhXJpislkKDnkSRwtKgDAiwbm1Z5aqYstkJtItfpWnDUufzpGFMUl_13mclCOjvI4M4Oce5k8RV1FYV8VDyW8
+-_y5My3JwSxv7w9xWvt7D3RTxXffhR6GvqddfKqtg2W-SsHVOg'''
 
 bot = Bot('my-bot')
 dispatcher = CliDispatcher(bot)
+vk_dispatcher = VkDispatcher(bot, vk_api_token)
 
 
 @bot.command('hello', description="Выводит в чат приветствие пользователя", example_output="Hello, {Username}")
@@ -19,4 +24,4 @@ def hello_handler(msg: Message, args: List[str]) -> str:
 
 
 if __name__ == '__main__':
-    dispatcher.start_polling()
+    vk_dispatcher.vk_start_pooling()
